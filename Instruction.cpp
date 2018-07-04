6a3d3780f1c64056bd7d12fab4a24c67a5896d11
lailanelkoussy <lailanelkoussy@aucegypt.edu>
2018-07-04 10:49:48 +0200

    instruction class added

---
+++ Instruction.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Laila Nasser ElKoussy 900160812 on 7/3/18.
+//
+
+#include "Instruction.h"
---
+++ Instruction.h
@@ -0,0 +1,18 @@
+//
+// Created by Laila Nasser ElKoussy 900160812 on 7/3/18.
+//
+
+#ifndef PROJECT_1_INSTRUCTION_H
+#define PROJECT_1_INSTRUCTION_H
+
+
+class Instruction {
+
+private:
+    unsigned int opcode, rd, ;
+
+
+};
+
+
+#endif //PROJECT_1_INSTRUCTION_H
